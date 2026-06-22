9b741021c9eb19edc3d0e70b11ee71283d1f7ba0
Altay Taneri <taneri862@gmail.com>
2026-06-22 13:55:55 +0300

    fix: update actuator HealthIndicator for Boot 4 package structure
    - Import from org.springframework.boot.health.contributor instead of org.springframework.boot.actuate.health

--- src/main/java/com/saveapenny/ocr/support/monitoring/OcrHealthIndicator.java
+++ src/main/java/com/saveapenny/ocr/support/monitoring/OcrHealthIndicator.java
@@ -3,8 +3,8 @@ package com.saveapenny.ocr.support.monitoring;
 import com.saveapenny.config.OcrProperties;
 import com.saveapenny.ocr.support.runtime.OcrRuntimeChecker;
 import com.saveapenny.ocr.support.runtime.OcrRuntimeStatus;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.health.contributor.Health;
+import org.springframework.boot.health.contributor.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 @Component
